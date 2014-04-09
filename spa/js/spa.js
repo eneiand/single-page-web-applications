fd6f5ec215d53c65ab3a62e30eff77254a19da76
Neil Anderson <neil.t.anderson@gmail.com>
2014-04-09 16:10:07 +0100

    added the login capability and Fake data provider

--- spa/js/spa.js
+++ spa/js/spa.js
@@ -12,8 +12,10 @@
 /*global $, spa */
 
 var spa = (function () {
+  'use strict';
   var initModule = function ( $container ) {
-     spa.shell.initModule( $container );
+    spa.model.initModule(); 
+    spa.shell.initModule( $container );
   };
 
   return { initModule: initModule };
